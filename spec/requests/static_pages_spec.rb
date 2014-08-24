956b15d1e9d0c7e470c4e1d753df9746e8cb9c71
bsborges <b.borges@gmail.com>
2014-08-24 17:47:05 +0000

    Update rspec tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  
-  let(:title) { "Ruby on Rails Tutorial Sample App" }
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
 
@@ -13,7 +13,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title(title + " | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   
@@ -26,7 +26,7 @@ describe "Static pages" do
     
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title(title + " | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   
@@ -39,7 +39,8 @@ describe "Static pages" do
     
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title(title + " | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+      
     end
   end
   
@@ -52,7 +53,7 @@ describe "Static pages" do
     
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title(title + " | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
   
